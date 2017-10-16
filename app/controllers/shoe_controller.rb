0fb124334689815cabbefcaf3dd8f55a9b7e51fa
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-10-15 18:47:58 -0700

    Updated Shoe & User Contoller. Added Methods.

---
+++ app/controllers/shoe_controller.rb
@@ -0,0 +1,22 @@
+class ShoeController < Sinatra::Base
+  get '/tweets' do
+  end
+
+  get '/tweets/new' do
+  end
+
+  post '/tweets/new' do
+  end
+
+  get '/tweets/:id' do
+  end
+
+  get '/tweets/:id/edit' do
+  end
+
+  patch '/tweets/:id' do
+  end
+
+  delete '/tweets/:id/delete' do
+  end
+end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,19 @@
+class UserController < Sinatra::Base
+  get '/signup' do
+  end
+
+  post '/signup' do
+  end
+
+  get '/login' do
+  end
+
+  post '/login' do
+  end
+
+  get '/logout' do
+  end
+
+  get '/users/:slug' do
+  end
+end
