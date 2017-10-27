1009d4f9713a0383288586dd3ceaa0bacc3fbce0
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-10-26 20:16:59 -0700

    Done.

--- app/controllers/shoe_controller.rb
+++ app/controllers/shoe_controller.rb
@@ -67,7 +67,7 @@ class ShoeController < ApplicationController
     @shoe_slug = "#{@shoe.brand} #{@shoe.style_name} #{@shoe.color}".downcase.gsub(' ','+')
     url = "https://www.google.com/search?biw=1680&bih=976&tbm=isch&sa=1&q=" + @shoe_slug + "&oq=" + @shoe_slug + "&gs_l=psy-ab.3..0l10.4067.7152.0.7267.19.19.0.0.0.0.120.1489.17j2.19.0....0...1.1.64.psy-ab..0.19.1488...0i67k1.0.J04MymRcUzg"
     ReleaseScraper.scrape_shoe(url)
-    @url = ReleaseScraper.url
+    @url = ReleaseScraper.image_url
     @shoe.url = @url
     @shoe.user_id = current_user.id
     @shoe.save
